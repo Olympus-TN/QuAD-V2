08c33905af0403b26561eb787b95689b74fd0926
GhassenEljday <ghassenjday2001@gmail.com>
2020-10-06 00:50:03 +0100

    first page are ready

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,26 +1,28 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Quad</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+<nav id="navbar" class="navbar navbar-expand-lg navbar-light" :class="navbar">
+  <a id="logo" class="navbar-brand" href="#">Quad</a>
+  <button class="navbar-toggler" type="button" data-toggle="Collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a id="about" class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="#">About us <span class="sr-only">(current)</span></a>
+        <a id="about" class="nav-link" href="#">About us <span class="sr-only">(current)</span></a>
       </li>
        <li class="nav-item active">
-        <a class="nav-link" href="#">Contact <span class="sr-only">(current)</span></a>
+        <a id="about" class="nav-link" href="#">Contact <span class="sr-only">(current)</span></a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
+        <div class="collapse multi-collapse" id="tog" >
       <input class="form-control mr-sm-2" type="email" placeholder="Email">
       <input class="form-control mr-sm-2" type="password" placeholder="Password">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Log in</button>
+      </div>
+      <button  id ="btn" class="btn btn-primary"  type="submit" data-toggle="collapse" data-target="#btn, #tog" >Log in</button>
     </form>
   </div>
 </nav>
@@ -31,4 +33,15 @@ export default {
 };
 </script>
 <style scoped>
+#about{
+    color: white;
+    font-size: 20px;
+}
+
+#logo{
+    color: white;
+    font-size: 34px;
+
+}
+
 </style>
