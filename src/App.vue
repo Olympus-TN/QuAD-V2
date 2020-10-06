cf1e8106513019c8e69d98473aa8a1576aeeda15
Med-Amine-Louati <medamine505@live.fr>
2020-10-06 14:43:37 +0100

    add new job

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <div class="appDiv">
-    <navbar />
-    <signup />
+    <!-- <navbar />
+    <signup /> -->
+    <postJob />
   </div>
 </template>
 
 <script>
-import signup from "./components/signup.vue";
-import navbar from "./components/navbar.vue";
+// import signup from "./components/signup.vue";
+// import navbar from "./components/navbar.vue";
+import postJob from "./components/company/postJob.vue"
 
 export default {
   name: "App",
   components: {
-    signup,
-    navbar,
+      postJob
+    // signup,
+    // navbar,
   },
 };
 </script>
 
 <style scoped>
-.appDiv {
+/* .appDiv {
   background-image: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("https://cdn.mos.cms.futurecdn.net/kVHndERw4LxuWxArZouVU6.jpg");
 ;
   background-size: cover;
   height: 805px;
   font-family: "Varela Round", sans-serif;
-}
+} */
 </style>
